b66ca91af87b8255e629d3aa162d1cbce90e124c
Eduardo <130085121+dudeksx@users.noreply.github.com>
2024-10-24 21:08:03 -0300

    Other 2 simple challenges

---
+++ Desafio 02 - Formatar nascimento.py
@@ -0,0 +1,9 @@
+#Desafio 2
+
+print('Seja bem-vindo(a)!')
+
+dia = input('Por favor, digite o dia do seu nascimento! ')
+mes = input('Por favor, digite o mês do seu nascimento! ')
+ano = input('Por favor, digite o ano do seu nascimento! ')
+
+print('Você nasceu em: ', dia, '/', mes, '/',ano)
---
+++ Desafio 03 -  soma 2 números.py
@@ -0,0 +1,10 @@
+#Desafio 3
+
+print('Seja bem-vindo(a)!')
+
+n1 = int(input('Digite o primeiro número a ser somado: '))
+n2 = int(input('Digite o segundo número a ser somado: '))
+
+resultado = n1+n2
+
+print('A soma dos dois números é: ', resultado)
